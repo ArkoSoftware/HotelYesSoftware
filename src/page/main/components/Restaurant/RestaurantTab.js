941545f640d82964b891c863ccd7b3cce96044a1
indrajith76 <indrajithgoswami@gmail.com>
2023-02-22 14:53:13 +0600

    Admin Access > Restaurants > The three dots menu section should contain a delete and edit options.

--- src/page/main/components/Restaurant/RestaurantTab.js
+++ src/page/main/components/Restaurant/RestaurantTab.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { IoEllipsisVertical } from "react-icons/io5";
 import { getTableList } from "./functions/function";
-import { extreSmallFont, mediumFont } from "../../../../theme";
+import { mediumFont } from "../../../../theme";
 import Loader from "../../../../components/Loader/Loader";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
 export const RestaurantTab = ({ rerender }) => {
   const [tableData, setTableData] = React.useState([]);
@@ -30,16 +31,35 @@ export const RestaurantTab = ({ rerender }) => {
 
 export const RestaurantCard = ({ item }) => {
   return (
-    <div className="bg-gray-200 w-24 h-24 rounded-2xl flex flex-col p-4">
-      <div className="ml-auto">
-        <IoEllipsisVertical size={12} />
+    <>
+      <div className="bg-gray-200 w-24 h-24 rounded-2xl flex flex-col p-4">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="absolute right-0">
+            <IoEllipsisVertical size={12} />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box"
+          >
+            <li>
+              <button className="text-sm px-2 py-1 text-green-600 hover:bg-green-600 hover:text-white">
+                <FaEdit />Edit
+              </button>
+            </li>
+            <li>
+              <button className="text-sm px-2 py-1 text-red-600 hover:bg-red-600 hover:text-white">
+                <FaTrashAlt />Delete
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div
+          className="text-2xl text-center my-auto"
+          style={{ fontSize: mediumFont }}
+        >
+          {item.tableNumber}
+        </div>
       </div>
-      <div
-        className="text-2xl text-center my-auto"
-        style={{ fontSize: mediumFont }}
-      >
-        {item.tableNumber}
-      </div>
-    </div>
+    </>
   );
 };

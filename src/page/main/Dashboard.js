28f58d0d290b02c2ffe47f2a3d1d70ef42efc8aa
indrajith76 <indrajithgoswami@gmail.com>
2023-02-26 14:23:01 +0600

    day begin and connect repo

--- src/page/main/Dashboard.js
+++ src/page/main/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
+    //dashboard
     <div className="p-5">
       <div className="p-9 font-bold text-2xl tracking-tighter">Dashboard</div>
     </div>

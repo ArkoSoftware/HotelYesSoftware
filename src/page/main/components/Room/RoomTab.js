4a44162f97e2b4075d34f7f5167d64d0da035f41
indrajith76 <indrajithgoswami@gmail.com>
2023-02-26 22:07:13 +0600

    added modal and pass modal selected data

--- src/page/main/components/Room/RoomTab.js
+++ src/page/main/components/Room/RoomTab.js
@@ -4,6 +4,7 @@ import { extreSmallFont, largeFont } from "../../../../theme";
 import Loader from "../../../../components/Loader/Loader";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import RoomUpdateModal from "./RoomUpdateModal";
+import { useState } from "react";
 
 export const RoomTab = ({ rerender, roomdata }) => {
   return (
@@ -16,65 +17,70 @@ export const RoomTab = ({ rerender, roomdata }) => {
           </div>
         ))}
       </div>
-      <RoomUpdateModal />
     </>
   );
 };
 
 export const RoomCard = ({ item }) => {
-  return (
-    <div className="bg-gray-200 w-40 md:w-44 h-40 md:h-44 rounded-2xl flex flex-col p-4">
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="absolute right-0">
-          <IoEllipsisVertical size={12} />
-        </label>
-        <ul
-          tabIndex={0}
-          className="dropdown-content menu p-2 shadow bg-base-100 rounded-box mr-3"
-        >
-          <li>
-            <label
-              htmlFor="editRoomModal"
-              className="text-sm px-2 py-1 text-green-600 hover:bg-green-600 hover:text-white"
-            >
-              <FaEdit />
-              Edit
-            </label>
-          </li>
-          <li>
-            <button className="text-sm px-2 py-1 text-red-600 hover:bg-red-600 hover:text-white">
-              <FaTrashAlt />
-              Delete
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div
-        className="text-2xl text-center my-4"
-        style={{ fontSize: largeFont }}
-      >
-        {item.roomNumber}
-      </div>
-      {item.type == "Premium" ? (
-        <div
-          className="capitalize mt-auto bg-yellow-500 px-3 py-1 rounded-xl "
-          style={{ alignSelf: "flex-start", fontSize: extreSmallFont }}
-        >
-          {item.type}
-        </div>
-      ) : (
-        <div
-          className="capitalize mt-auto bg-white text-black px-3 py-1 rounded-xl"
-          style={{ alignSelf: "flex-start", fontSize: extreSmallFont }}
-        >
-          {item.type}
-        </div>
-      )}
+  const [data, setData] = useState({});
 
-      <div className="flex m-1 mt-2" style={{ fontSize: extreSmallFont }}>
-        <div className="mr-2 ">Price:</div>
-        <div className="">Rs.{item.price}</div>
+  return (
+    <>
+      <div className="bg-gray-200 w-40 md:w-44 h-40 md:h-44 rounded-2xl flex flex-col p-4">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="absolute right-0">
+            <IoEllipsisVertical size={12} />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box mr-3"
+          >
+            <li>
+              <label
+                htmlFor="editRoomModal"
+                className="text-sm px-2 py-1 text-green-600 hover:bg-green-600 hover:text-white"
+                onClick={() => setData(item)}
+              >
+                <FaEdit />
+                Edit
+              </label>
+            </li>
+            <li>
+              <button className="text-sm px-2 py-1 text-red-600 hover:bg-red-600 hover:text-white">
+                <FaTrashAlt />
+                Delete
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div
+          className="text-2xl text-center my-4"
+          style={{ fontSize: largeFont }}
+        >
+          {item.roomNumber}
+        </div>
+        {item.type == "Premium" ? (
+          <div
+            className="capitalize mt-auto bg-yellow-500 px-3 py-1 rounded-xl "
+            style={{ alignSelf: "flex-start", fontSize: extreSmallFont }}
+          >
+            {item.type}
+          </div>
+        ) : (
+          <div
+            className="capitalize mt-auto bg-white text-black px-3 py-1 rounded-xl"
+            style={{ alignSelf: "flex-start", fontSize: extreSmallFont }}
+          >
+            {item.type}
+          </div>
+        )}
+
+        <div className="flex m-1 mt-2" style={{ fontSize: extreSmallFont }}>
+          <div className="mr-2 ">Price:</div>
+          <div className="">Rs.{item.price}</div>
+        </div>
       </div>
-    </div>
+      <RoomUpdateModal data={data} setData={setData} />
+    </>
   );
 };
--- src/page/main/components/Room/RoomUpdateModal.js
+++ src/page/main/components/Room/RoomUpdateModal.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-const RoomUpdateModal = () => {
+const RoomUpdateModal = ({ data }) => {
+  const { type, roomNumber, time, user, price, id } = data;
+
+  
+
   return (
     <div>
       <input type="checkbox" id="editRoomModal" className="modal-toggle" />
@@ -12,21 +16,56 @@ const RoomUpdateModal = () => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">Update Room Data</h3>
+          <h3 className="text-lg font-normal">Update Room Data</h3>
           <div className="py-4">
-            <label htmlFor="RoomNumber">Room Number</label>
+            <label className="block text-xs" htmlFor="RoomNumber">
+              Room Number
+            </label>
+
             <input
               type="text"
               name="RoomNumber"
-              id=""
+              id="RoomNumber"
+              value={roomNumber}
               placeholder="Room Number"
+              className="input input-bordered input-sm w-full mb-4 mt-2"
             />
-            <label htmlFor="Price">Price</label>
-            <input type="text" name="Price" id="Price" placeholder="Price" />
-            <label htmlFor="RoomType">Room Type</label>
-            <select name="RoomType" id="RoomType">
-
+            <label className="block text-xs" htmlFor="Price">
+              Price
+            </label>
+            <input
+              type="text"
+              name="Price"
+              id="Price"
+              placeholder="Price"
+              value={price}
+              className="input input-bordered input-sm w-full mb-4 mt-2"
+            />
+            <label className="block text-xs" htmlFor="RoomType">
+              Room Type
+            </label>
+            <select
+              name="RoomType"
+              id="RoomType"
+              className="input input-sm input-bordered w-full mt-2"
+            >
+              <option disabled selected>
+                {type ? type : "Choose one"}
+              </option>
+              {[
+                "King Size",
+                "Double + Single Bed",
+                "3 Single Bed",
+                "2 Double Bed",
+                "2 Single Bed",
+              ].map((opt) => (
+                <option value={opt}>{opt}</option>
+              ))}
             </select>
+
+            <button className="bg-green-600 text-white mt-5 py-1 rounded-md w-full">
+              Update Room
+            </button>
           </div>
         </div>
       </div>
